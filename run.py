48839883a65c2b581b2a6bc66e0d30b82d32e95f
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-06-15 16:09:59 +0000

    put dependencies in requirements.txt

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import os
 import json
-from flask import Flask, render_template,request, flash
-
+from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
+
 
 @app.route("/")
 def index():
@@ -20,6 +20,7 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
 
 @app.route("/about/<member_name>")
 def about_member(member_name):
@@ -43,7 +44,6 @@ def contact():
 @app.route("/careers")
 def careers():
     return render_template("careers.html", page_title="Careers")
-
 
 
 if __name__ == "__main__":
